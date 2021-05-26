f52cbcf74de41e832af403e1878f49921bac8d41
olivier-vadiaval <vadiaval@ualberta.ca>
2021-05-26 14:27:39 -0600

    Create the AST node classes for the DSFs and DSBs
     - Create any missing sub-classes that derive from the Node base class.
    
     - So far, only the If-then, If-then-Else and ReturnAction AST node classes
       have been created.
    
     - The implementations for domain-specific functions (DSFs) and domain-
       specific booleans (DSBs) were missing.
    
     - In particular, classes for returning the player's position and the
       falling fruit's position are missing.
    
     - Moreover, classes implementing simple arithmetic operators (such as
       Plus, Minus, Times) and DSBs (such as comparison operators '<', '>',
       '==') were not implemented in the DSL module.
    
     - Implement those classes with only their __init__ and toString methods for now.

--- DSL.py
+++ DSL.py
@@ -108,3 +108,209 @@ class ITE(Node):
         ite_string += f"""{tab}else:\n\t"""
         ite_string += f"""{self.else_body.toString()}"""
         return ite_string
+
+
+"""
+This class implements a domain-specific function that returns
+the x-position of the player on the screen.
+"""
+class PlayerPosition(Node):
+
+    def __init__(self):
+        super(PlayerPosition, self).__init__()
+        self.size = 1
+
+    def toString(self):
+        return PlayerPosition.className()
+
+
+"""
+This class implements a domain-specific function that returns
+the y-position of the falling fruit to be caught by the player.
+"""
+class FallingFruitPosition(Node):
+
+    def __init__(self):
+        super(FallingFruitPosition, self).__init__()
+        self.size = 1
+
+    def toString(self):
+        return FallingFruitPosition.className()
+
+
+"""
+This class implements an AST node representing a domain-specific scalar variable.
+For instance, the player's paddle width.
+"""
+class VarScalar(Node):
+
+    def __init__(self, name):
+        super(VarScalar, self).__init__()
+        self.size = 1
+        self.name = name
+
+    def toString(self):
+        return f"{self.name}"
+
+
+"""
+This class implements an AST node representing a domain-specific variable from
+an array. For example, actions[0]
+"""
+class VarFromArray(Node):
+
+    def __init__(self, name, index):
+        super(VarFromArray, self).__init__()
+        self.name = name
+        self.index = index
+    
+    def toString(self):
+        return f"{self.name}[{self.index}]"
+
+
+"""
+This class implements an AST node representing the '<' comparison
+operator. It returns either True or False on calling its interpret
+method.
+"""
+class LessThan(Node):
+
+    def __init__(self, left, right):
+        super(LessThan, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} < {self.right}"
+
+
+"""
+This class implements an AST node representing the '>' comparison
+operator. It returns either True or False on calling its interpret
+method.
+"""
+class GreaterThan(Node):
+
+    def __init__(self, left, right):
+        super(GreaterThan, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} > {self.right}"
+
+
+"""
+This class implements an AST node representing the '==' comparison
+operator
+"""
+class EqualTo(Node):
+
+    def __init__(self, left, right):
+        super(EqualTo, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} == {self.right}"
+
+
+"""
+This class implements an AST node representing the addition operator.
+"""
+class Plus(Node):
+
+    def __init__(self, left, right):
+        super(Plus, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} + {self.right}"
+
+
+"""
+This class implements an AST node representing the multiplication operator
+"""
+class Times(Node):
+
+    def __init__(self, left, right):
+        super(Times, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} * {self.right}"
+
+
+"""
+This class implements an AST node representing the minus operator
+"""
+class Minus(Node):
+
+    def __init__(self, left, right):
+        super(Minus, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} - {self.right}"
+
+
+"""
+This class implements an AST node representing the integer division operator
+"""
+class Division(Node):
+
+    def __init__(self, left, right):
+        super(Division, self).__init__()
+        self.size = 1 + left.getSize() + right.getSize()
+        self.left = left
+        self.right = right
+
+    def toString(self):
+        return f"{self.left} // {self.right}"
+
+
+"""
+This class implements an AST node representing a constant.
+"""
+class Constant(Node):
+
+    def __init__(self, value):
+        super(Constant, self).__init__()
+        assert value in range(101)
+        self.size = 1
+        self.value = value
+
+    def toString(self):
+        return f"{self.value}"
+
+
+"""
+This class implements the initial symbol of the DSL.
+"""
+class Strategy(Node):
+
+    def __init__(self, statement, next_statements):
+        super(Strategy, self).__init__()
+        assert type(statement).__name__ in [IT.className(), ITE.className()]
+        assert type(next_statements).__name__ == Strategy.className() or next_statements is None
+        self.size = statement.getSize()
+        if next_statements is not None:
+            self.size += next_statements.getSize()
+        self.statement = statement
+        self.next_statements = next_statements
+
+    def toString(self):
+        strategy_string = f"{self.statement.toString()}\n"
+        if self.next_statements is not None:
+            strategy_string += f"{self.next_statements.toString()}"
+
+        return strategy_string
