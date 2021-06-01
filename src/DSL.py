2b6276787e19ae81d6afd087b5597959bf706860
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-01 09:40:52 -0600

    Add grow method for the Divide class in DSL.py

--- src/DSL.py
+++ src/DSL.py
@@ -465,6 +465,34 @@ class Divide(Node):
     def interpret(self, env):
         return self.left.interpret(env) // self.right.interpret(env)
 
+    def grow(plist, psize):
+        nplist = []
+        valid_nodes = [VarScalar.className(), PlayerPosition.className(), FallingFruitPosition.className(),
+            Constant.className(), Times.className(), Minus.className(), Plus.className(), Divide.className()]
+
+        cost_combinations = itertools.product(range(psize-1), repeat=2)
+
+        for cost in cost_combinations:
+            if cost[0] + cost[1] + 1 == psize:
+                program_set_1 = plist.get(cost[0])
+                program_set_2 = plist.get(cost[1])
+
+                if program_set_1 is not None and program_set_2 is not None:
+                    for t1, p1 in program_set_1.items():
+                        if t1 in valid_nodes:
+                            for left in p1:
+                                
+                                for t2, p2 in program_set_2.items():
+                                    if t2 in valid_nodes:
+                                        for right in p2:
+                                            if right.toString() != '0' and left.toString() != '0':
+                                                if left.toString() != right.toString():
+                                                    divide = Divide(left, right)
+                                                    nplist.append(divide)
+                                                    yield divide
+        
+        return nplist
+
 
 """
 This class implements the initial symbol of the DSL.
