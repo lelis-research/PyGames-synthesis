2ef6225ef1b45a24a650d69e1237b34d9508f24a
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-02 14:19:42 -0600

    Replace init_env with setUp method in test_dsl_dsfs.py

--- tests/test_dsl_dsfs.py
+++ tests/test_dsl_dsfs.py
@@ -1,21 +1,19 @@
 import unittest
 from unittest.loader import makeSuite
-from DSL import PlayerPosition, FallingFruitPosition
+from src.DSL import PlayerPosition, FallingFruitPosition
 
 class TestPlayerPosition(unittest.TestCase):
 
-    def init_env(self):
+    def setUp(self):
         self.env = {}
         self.env['state'] = {}
         self.env['state']['player_position'] = 60
 
     def test_interpret_no_exception(self):
-        self.init_env()
         p_position = PlayerPosition()
         self.assertEqual(p_position.interpret(self.env), 60, 'interpret method of PlayerPosition object should return 60')
 
     def test_interpret_key_error(self):
-        self.init_env()
         p_position = PlayerPosition()
         self.env['state'].pop('player_position')
         test_msg = 'interpret method of PlayerPosition object should raise KeyError'
@@ -26,18 +24,16 @@ class TestPlayerPosition(unittest.TestCase):
 
 class TestFallingFruitPosition(unittest.TestCase):
 
-    def init_env(self):
+    def setUp(self):
         self.env = {}
         self.env['state'] = {}
         self.env['state']['fruit_position'] = 55
 
     def test_interpret_no_exception(self):
-        self.init_env()
         p_position = FallingFruitPosition()
         self.assertEqual(p_position.interpret(self.env), 55, 'interpret method of FallingFruitPosition object should return 60')
 
     def test_interpret_key_error(self):
-        self.init_env()
         p_position = FallingFruitPosition()
         self.env['state'].pop('fruit_position')
         test_msg = 'interpret method of FallingFruitPosition object should raise KeyError'
