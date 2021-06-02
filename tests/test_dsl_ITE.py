eb6701a60e07af32fde128059206b7f244887173
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-02 13:29:29 -0600

    Replace IF_COND and BODY classes with mock objects

--- tests/test_dsl_ITE.py
+++ tests/test_dsl_ITE.py
@@ -1,42 +1,32 @@
 import unittest
-from DSL import ITE, ReturnAction
+from src.DSL import ITE, ReturnAction
+from unittest.mock import Mock
 
 class TestITE(unittest.TestCase):
 
-    class IF_COND:
-
-        def __init__(self, cond):
-            self.size = 1
-            self.cond = cond
-
-        def getSize(self):
-            return self.size
-
-        def interpret(self, env):
-            return env[self.cond]
-
-    class BODY:
-
-        def __init__(self, body):
-            self.size = 1
-            self.body = body
-            self.__class__.__name__ = 'ReturnAction'
-
-        def getSize(self):
-            return self.size
-
-        def interpret(self, env):
-            return env[self.body]
-
-    def init_env(self):
+    def setUp(self):
         self.env = {}
         self.env['TRUE'] = True
         self.env['FALSE'] = False
         self.env['IF_BODY'] = 100
         self.env['ELSE_BODY'] = 53
+        
+        self.if_cond = Mock()
+        self.if_body = Mock()
+        self.else_body = Mock()
+
+        type(self.if_body).__name__ = 'ReturnAction'
+        type(self.else_body).__name__ = 'ReturnAction'
+
+        self.if_cond.getSize.return_value = 1
+        self.if_body.getSize.return_value = 1
+        self.else_body.getSize.return_value = 1
 
     def init_ITE(self, if_cond, if_body, else_body):
-        return ITE(TestITE.IF_COND(if_cond), TestITE.BODY(if_body), TestITE.BODY(else_body))
+        self.if_cond.interpret.return_value = self.env[if_cond]
+        self.if_body.interpret.return_value = self.env[if_body]
+        self.else_body.interpret.return_value = self.env[else_body]
+        return ITE(self.if_cond, self.if_body, self.else_body)
 
     def test_size_four(self):
         ite = self.init_ITE('TRUE', 'IF_BODY', 'ELSE_BODY')
@@ -50,26 +40,20 @@ class TestITE(unittest.TestCase):
         self.assertEqual(ite.getSize(), 100, 'ITE object should have size 100')
 
     def test_size_change_arg_size(self):
-        if_cond = TestITE.IF_COND('TRUE')
-        if_body = TestITE.BODY('IF_BODY')
-        else_body = TestITE.BODY('ELSE_BODY')
-
         # Manually change the size of the arguments to the ITE constructor
-        if_cond.size = 10
-        if_body.size = 10
-        else_body.size = 10
-        ite = ITE(if_cond, if_body, else_body)
-        self.assertEqual(ite.getSize(), 31, 'ITE object should have size 31')
+        ite = ITE(self.if_cond, self.if_body, self.else_body)
+        ite.condition.getSize.return_value = 10
+        ite.if_body.getSize.return_value = 10
+        ite.else_body.getSize.return_value = 10
+        self.assertEqual(ite.getSize(), 4, 'ITE object should have size 4')
 
     def test_interpret_false(self):
         # Test the interpret method with the if-condition evaluating to False
-        self.init_env()
         ite = self.init_ITE('FALSE', 'IF_BODY', 'ELSE_BODY')
         self.assertEqual(ite.interpret(self.env), 53, 'interpret method of ITE object should return 53')
 
     def test_interpret_true(self):
         # Test the interpret method with the if-condition evaluating to True
-        self.init_env()
         ite = self.init_ITE('TRUE', 'IF_BODY', 'ELSE_BODY')
         self.assertEqual(ite.interpret(self.env), 100, 'interpret method of ITE object should return 100')
 
