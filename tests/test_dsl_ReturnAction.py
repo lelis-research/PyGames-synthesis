a0f37da30d08dae3dea889b5e1c65f03c0b96d52
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-02 14:11:42 -0600

    Replace action with mockAction class in test_dsl_ReturnAction.py

--- tests/test_dsl_ReturnAction.py
+++ tests/test_dsl_ReturnAction.py
@@ -1,56 +1,53 @@
 import unittest
-from DSL import *
+from src.DSL import *
+from unittest.mock import Mock
 
 class TestReturnAction(unittest.TestCase):
 
-    class Action:
-
-        def __init__(self, action):
-            self.size = 1
-            self.action = action
-
-        def getSize(self):
-            return self.size
-
-        def interpret(self, env):
-            return env['action'][self.action]
-
-    def init_env(self):
+    def setUp(self):
         self.env = {}
         self.env['action'] = {}
         self.env['action']['LEFT'] = 5
         self.env['action']['RIGHT'] = 48
 
-    def test_size(self):
+    def mockAction(self, value):
+        action = Mock()
+        action.getSize.return_value = 1
+        action.interpret.return_value = self.env['action'][value]
+        return action
+
+    def test_size_two(self):
         # Test with action object of size 1
         # ret_action should return 1 + action.getSize() = 1 + 1 = 2
-        action = TestReturnAction.Action('LEFT')
+        action = self.mockAction('LEFT')
         ret_action = ReturnAction(action)
-        self.assertEqual(ret_action.getSize(), 2, '#1 ReturnAction object should have size 2')
+        self.assertEqual(ret_action.getSize(), 2, 'ReturnAction object should have size 2')
         
-        # Test action object with modified getSize method
-        # action.getSize method always returns a size of 5 regardless of self.size's value
-        TestReturnAction.Action.getSize = lambda x : 5
-        action = TestReturnAction.Action('LEFT')
+    def test_size_change_arg_size(self):
+        # Manually change the mock action's size
+        action = self.mockAction('LEFT')
         ret_action = ReturnAction(action)
-        self.assertEqual(ret_action.getSize(), 6, '#2 ReturnAction object should have size 6')
+        action.getSize.return_value = 100
+        self.assertEqual(ret_action.getSize(), 2, 'ReturnAction object should have size 2')
 
+    def test_size_change_action_size(self):
         # set action size to 1000
         # action.getSize should still return 5 because getSize was changed to lambda x : 5
-        action.size = 1000
-        self.assertEqual(ret_action.getSize(), 6, '#3 ReturnAction object should size 6')
+        ret_action = ReturnAction(self.mockAction('LEFT'))
+        ret_action.size = 1000
+        self.assertEqual(ret_action.getSize(), 1000, 'ReturnAction object should size 1000')
 
-    def test_interpret(self):
+    def test_interpret_left(self):
         # Test with action object with action string equal to 'LEFT'
-        self.init_env()
-        action = TestReturnAction.Action('LEFT')
+        action = self.mockAction('LEFT')
         ret_action = ReturnAction(action)
-        self.assertEqual(ret_action.interpret(self.env), 5, '#1 interpret method of ReturnAction object should return 5')
+        self.assertEqual(ret_action.interpret(self.env), 5, 'interpret method of ReturnAction object should return 5')
 
+    def test_interpret_right(self):
         # Test with action object with action string equal to 'RIGHT'
-        action = TestReturnAction.Action('RIGHT')
+        action = self.mockAction('RIGHT')
         ret_action = ReturnAction(action)
-        self.assertEqual(ret_action.interpret(self.env), 48, '#2 interpret method of ReturnAction object should return 48')
+        self.assertEqual(ret_action.interpret(self.env), 48, 'interpret method of ReturnAction object should return 48')
 
 
 if __name__ == '__main__':
