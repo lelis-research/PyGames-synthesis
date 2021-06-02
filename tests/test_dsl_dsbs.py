6bc88cb0c5ad08bb4cc65f7c0750e85d3d888964
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-02 14:26:32 -0600

    Replace Operand class with mock object in test_dsl_dsbs.py

--- tests/test_dsl_dsbs.py
+++ tests/test_dsl_dsbs.py
@@ -1,121 +1,116 @@
 import unittest
-from DSL import LessThan, GreaterThan, EqualTo
+from src.DSL import LessThan, GreaterThan, EqualTo
+from unittest.mock import Mock
 
-class Operand:
-
-    def __init__(self, value):
-        self.size = 1
-        self.value = value
-    
-    def getSize(self):
-        return self.size
-
-    def interpret(self, env):
-        return self.value
+def mockOperand(value):
+    op = Mock()
+    op.getSize.return_value = 1
+    op.interpret.return_value = value
+    return op
 
 
 class TestLessThan(unittest.TestCase):
 
     def test_size_three(self):
-        lt = LessThan(Operand(5), Operand(3))
+        lt = LessThan(mockOperand(5), mockOperand(3))
         self.assertEqual(lt.getSize(), 3, 'LessThan object should have size 3')
 
     def test_size_change_lessThan_size(self):
         # Manually change lt.size to 100
-        lt = LessThan(Operand(5), Operand(5))
+        lt = LessThan(mockOperand(5), mockOperand(5))
         lt.size = 100
         self.assertEqual(lt.getSize(), 100, 'LessThan object should have size 100')
 
     def test_size_change_args_size(self):
         # Manually change the Operand sizes
-        left = Operand(5)
-        left.size = 50
-        right = Operand(14)
-        right.size = 10
+        left = mockOperand(5)
+        right = mockOperand(14)
         lt = LessThan(left, right)
-        self.assertEqual(lt.getSize(), 61, 'LessThan object should have size 61')
+        left.getSize.return_value = 10
+        right.getSize.return_value = 10
+        self.assertEqual(lt.getSize(), 3, 'LessThan object should have size 3')
 
     def test_interpret_true(self):
-        lt = LessThan(Operand(3), Operand(10))
+        lt = LessThan(mockOperand(3), mockOperand(10))
         self.assertEqual(type(lt.interpret({})).__name__, 'bool', 'interpret method of LessThan object should return a \'bool\'')
         self.assertEqual(lt.interpret({}), True, 'interpret method of LessThan object should return True')
 
     def test_interpret_false(self):
-        lt = LessThan(Operand(88), Operand(10))
+        lt = LessThan(mockOperand(88), mockOperand(10))
         self.assertEqual(type(lt.interpret({})).__name__, 'bool', 'interpret method of LessThan object should return a \'bool\'')
         self.assertEqual(lt.interpret({}), False, 'interpret method of LessThan object should return False')
 
-        lt = LessThan(Operand(77), Operand(77))
+        lt = LessThan(mockOperand(77), mockOperand(77))
         self.assertEqual(lt.interpret({}), False, 'interpret method of LessThan object should return False')
 
 
 class TestGreaterThan(unittest.TestCase):
 
     def test_size_three(self):
-        gt = GreaterThan(Operand(5), Operand(3))
+        gt = GreaterThan(mockOperand(5), mockOperand(3))
         self.assertEqual(gt.getSize(), 3, 'GreaterThan object should have size 3')
 
     def test_size_change_lessThan_size(self):
         # Manually change gt.size to 100
-        gt = GreaterThan(Operand(5), Operand(5))
+        gt = GreaterThan(mockOperand(5), mockOperand(5))
         gt.size = 100
         self.assertEqual(gt.getSize(), 100, 'GreaterThan object should have size 100')
 
     def test_size_change_args_size(self):
         # Manually change the Operand sizes
-        left = Operand(5)
-        left.size = 50
-        right = Operand(14)
-        right.size = 10
+        left = mockOperand(5)
+        right = mockOperand(14)
         gt = GreaterThan(left, right)
-        self.assertEqual(gt.getSize(), 61, 'GreaterThan object should have size 61')
+        left.getSize.return_value = 10
+        right.getSize.return_value = 14
+        self.assertEqual(gt.getSize(), 3, 'GreaterThan object should have size 3')
 
     def test_interpret_true(self):
-        gt = GreaterThan(Operand(10), Operand(3))
+        gt = GreaterThan(mockOperand(10), mockOperand(3))
         self.assertEqual(type(gt.interpret({})).__name__, 'bool', 'interpret method of GreaterThan object should return a \'bool\'')
         self.assertEqual(gt.interpret({}), True, 'interpret method of GreaterThan object should return True')
 
     def test_interpret_false(self):
-        gt = GreaterThan(Operand(10), Operand(88))
+        gt = GreaterThan(mockOperand(10), mockOperand(88))
         self.assertEqual(type(gt.interpret({})).__name__, 'bool', 'interpret method of GreaterThan object should return a \'bool\'')
         self.assertEqual(gt.interpret({}), False, 'interpret method of GreaterThan object should return False')
 
-        gt = GreaterThan(Operand(77), Operand(77))
+        gt = GreaterThan(mockOperand(77), mockOperand(77))
         self.assertEqual(gt.interpret({}), False, 'interpret method of GreaterThan object should return False')
 
 
 class TestEqualTo(unittest.TestCase):
 
     def test_size_three(self):
-        eq = EqualTo(Operand(5), Operand(3))
+        eq = EqualTo(mockOperand(5), mockOperand(3))
         self.assertEqual(eq.getSize(), 3, 'EqualTo object should have size 3')
 
     def test_size_change_lessThan_size(self):
         # Manually change eq.size to 100
-        eq = EqualTo(Operand(5), Operand(5))
+        eq = EqualTo(mockOperand(5), mockOperand(5))
         eq.size = 100
         self.assertEqual(eq.getSize(), 100, 'EqualTo object should have size 100')
 
     def test_size_change_args_size(self):
-        # Manually change the Operand sizes
-        left = Operand(5)
-        left.size = 50
-        right = Operand(14)
-        right.size = 10
+        # Manually change the mockOperand sizes
+        left = mockOperand(5)
+        right = mockOperand(14)
         eq = EqualTo(left, right)
-        self.assertEqual(eq.getSize(), 61, 'EqualTo object should have size 61')
+        left.getSize.return_value = 50
+        right.getSize.return_value = 10
+        self.assertEqual(eq.getSize(), 3, 'EqualTo object should have size 3')
 
     def test_interpret_true(self):
-        eq = EqualTo(Operand(60), Operand(60))
+        eq = EqualTo(mockOperand(60), mockOperand(60))
         self.assertEqual(type(eq.interpret({})).__name__, 'bool', 'interpret method of EqualTo object should return a \'bool\'')
         self.assertEqual(eq.interpret({}), True, 'interpret method of EqualTo object should return True')
 
     def test_interpret_false(self):
-        eq = EqualTo(Operand(88), Operand(10))
+        eq = EqualTo(mockOperand(88), mockOperand(10))
         self.assertEqual(type(eq.interpret({})).__name__, 'bool', 'interpret method of EqualTo object should return a \'bool\'')
         self.assertEqual(eq.interpret({}), False, 'interpret method of EqualTo object should return False')
 
-        eq = EqualTo(Operand(45), Operand(92))
+        eq = EqualTo(mockOperand(45), mockOperand(92))
         self.assertEqual(eq.interpret({}), False, 'interpret method of EqualTo object should return False')
 
 
