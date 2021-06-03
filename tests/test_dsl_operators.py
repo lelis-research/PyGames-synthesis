a4cfc9108b3b5b5382227a808bf3c0a33a4a7390
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-03 15:25:28 -0600

    Refactor test_dsl_operators.py to use 'new' instead of __init__

--- tests/test_dsl_operators.py
+++ tests/test_dsl_operators.py
@@ -12,13 +12,13 @@ def mockOperand(value):
 class TestMinus(unittest.TestCase):
 
     def test_size_three(self):
-        minus = Minus(mockOperand(5), mockOperand(5))
+        minus = Minus.new(mockOperand(5), mockOperand(5))
         self.assertEqual(minus.getSize(), 3, 'Minus object should have size 3')
 
     def test_size_change_minus_size(self):
         # Manually change the size of the Minus object
         
-        minus = Minus(mockOperand(5), mockOperand(5))
+        minus = Minus.new(mockOperand(5), mockOperand(5))
         minus.size = 100
         self.assertEqual(minus.getSize(), 100, 'Minus object should have size 100')
 
@@ -26,25 +26,25 @@ class TestMinus(unittest.TestCase):
         # Manually change the sizes of the args to the Minus class constructor
         left = mockOperand(5)
         right = mockOperand(5)
-        minus = Minus(left, right)
-        left.getSize.return_value = 10
-        right.getSize.return_value = 10
+        minus = Minus.new(left, right)
+        minus.get_children()[0].getSize.return_value = 10
+        minus.get_children()[1].return_value = 10
         self.assertEqual(minus.getSize(), 3, 'Minus object should have size 3')
 
     def test_interpret(self):
-        minus = Minus(mockOperand(100), mockOperand(35))
+        minus = Minus.new(mockOperand(100), mockOperand(35))
         self.assertEqual(minus.interpret({}), 65, 'interpret method of Minus object should return 65')
 
 
 class TestAdd(unittest.TestCase):
 
     def test_size_three(self):
-        plus = Plus(mockOperand(5), mockOperand(5))
+        plus = Plus.new(mockOperand(5), mockOperand(5))
         self.assertEqual(plus.getSize(), 3, 'Plus object should have size 3')
 
     def test_size_change_minus_size(self):
         # Manually change the size of the Plus object
-        plus = Plus(mockOperand(5), mockOperand(5))
+        plus = Plus.new(mockOperand(5), mockOperand(5))
         plus.size = 100
         self.assertEqual(plus.getSize(), 100, 'Plus object should have size 100')
 
@@ -52,25 +52,25 @@ class TestAdd(unittest.TestCase):
         # Manually change the sizes of the args to the Plus class constructor
         left = mockOperand(5)
         right = mockOperand(5)
-        plus = Plus(left, right)
-        right.getSize.return_value = 10
-        left.getSize.return_value = 10
+        plus = Plus.new(left, right)
+        plus.get_children()[0].getSize.return_value = 10
+        plus.get_children()[1].getSize.return_value = 10
         self.assertEqual(plus.getSize(), 3, 'Plus object should have size 3')
 
     def test_interpret(self):
-        plus = Plus(mockOperand(100), mockOperand(35))
+        plus = Plus.new(mockOperand(100), mockOperand(35))
         self.assertEqual(plus.interpret({}), 135, 'interpret method of Plus object should return 135')
 
 
 class TestTimes(unittest.TestCase):
 
     def test_size_three(self):
-        times = Times(mockOperand(5), mockOperand(5))
+        times = Times.new(mockOperand(5), mockOperand(5))
         self.assertEqual(times.getSize(), 3, 'Times object should have size 3')
 
     def test_size_change_minus_size(self):
         # Manually change the size of the Times object
-        times = Times(mockOperand(5), mockOperand(5))
+        times = Times.new(mockOperand(5), mockOperand(5))
         times.size = 100
         self.assertEqual(times.getSize(), 100, 'Times object should have size 100')
 
@@ -78,25 +78,25 @@ class TestTimes(unittest.TestCase):
         # Manually change the sizes of the args to the Times class constructor
         left = mockOperand(5)
         right = mockOperand(5)
-        times = Times(left, right)
-        left.getSize.return_value = 10
-        right.getSize.return_value = 10
+        times = Times.new(left, right)
+        times.get_children()[0].getSize.return_value = 10
+        times.get_children()[1].getSize.return_value = 10
         self.assertEqual(times.getSize(), 3, 'Times object should have size 3')
 
     def test_interpret(self):
-        times = Times(mockOperand(100), mockOperand(35))
+        times = Times.new(mockOperand(100), mockOperand(35))
         self.assertEqual(times.interpret({}), 3500, 'interpret method of Times object should return 3500')
 
 
 class TestDivide(unittest.TestCase):
 
     def test_size_three(self):
-        divide = Divide(mockOperand(5), mockOperand(5))
+        divide = Divide.new(mockOperand(5), mockOperand(5))
         self.assertEqual(divide.getSize(), 3, 'Divide object should have size 3')
 
     def test_size_change_minus_size(self):
         # Manually change the size of the Divide object
-        divide = Divide(mockOperand(5), mockOperand(5))
+        divide = Divide.new(mockOperand(5), mockOperand(5))
         divide.size = 100
         self.assertEqual(divide.getSize(), 100, 'Divide object should have size 100')
 
@@ -104,21 +104,21 @@ class TestDivide(unittest.TestCase):
         # Manually change the sizes of the args to the Divide class constructor
         left = mockOperand(5)
         right = mockOperand(5)
-        divide = Divide(left, right)
-        left.getSize.return_value = 10
-        right.getSize.return_value = 10
+        divide = Divide.new(left, right)
+        divide.get_children()[0].getSize.return_value = 10
+        divide.get_children()[1].getSize.return_value = 10
         self.assertEqual(divide.getSize(), 3, 'Divide object should have size 3')
 
     def test_interpret_no_remainder(self):
-        divide = Divide(mockOperand(100), mockOperand(5))
+        divide = Divide.new(mockOperand(100), mockOperand(5))
         self.assertEqual(divide.interpret({}), 20, 'interpret method of Divide object should return 20')
 
     def test_interpret_with_remainder(self):
-        divide = Divide(mockOperand(100), mockOperand(3))
+        divide = Divide.new(mockOperand(100), mockOperand(3))
         self.assertEqual(divide.interpret({}), 33, 'interpret method of Divide object should return 33')
 
     def test_interpret_zero_division_error(self):
-        divide = Divide(mockOperand(100), mockOperand(0))
+        divide = Divide.new(mockOperand(100), mockOperand(0))
         test_msg = 'interpret method of Divide object should raise ZeroDivisionError'
         with self.assertRaises(ZeroDivisionError, msg=test_msg) as cm:
             divide.interpret({})
