b4d75e1edde8a9004db9935353dbfc1eb8e57ec3
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-03 15:32:34 -0600

    Modify test_dsl_strategy.py so that it uses 'new' instead of __init__

--- tests/test_dsl_strategy.py
+++ tests/test_dsl_strategy.py
@@ -21,15 +21,15 @@ def NextStatement(value):
 class TestStrategy(unittest.TestCase):
 
     def test_size_three(self):
-        s = Strategy(FirstStatement(90), NextStatement(100))
+        s = Strategy.new(FirstStatement(90), NextStatement(100))
         self.assertEqual(s.getSize(), 2, 'Strategy object should have size 3')
 
     def test_size_none_next_statement(self):
-        s = Strategy(FirstStatement(90), None)
+        s = Strategy.new(FirstStatement(90), None)
         self.assertEqual(s.getSize(), 1, 'Strategy object should have size 2')
 
     def test_size_change_strategy_size(self):
-        s = Strategy(FirstStatement(10), NextStatement(6))
+        s = Strategy.new(FirstStatement(10), NextStatement(6))
         s.size = 90
         self.assertEqual(s.getSize(), 90, 'Strategy object should have size 90')
 
@@ -38,32 +38,32 @@ class TestStrategy(unittest.TestCase):
         first_statement.__class__.__name__ = 'FirstStatement'
         test_msg = 'Strategy constructor should raise AssertionError'
         with self.assertRaises(AssertionError, msg=test_msg) as cm:
-            Strategy(first_statement, None)
+            Strategy.new(first_statement, None)
 
         first_statement.__class__.__name__ = IT.className()
         with self.assertRaises(AssertionError, msg=test_msg) as cm:
-            Strategy(first_statement, first_statement)
+            Strategy.new(first_statement, first_statement)
 
     def test_interpret_first_statement_only(self):
         first = FirstStatement(60)
         second = NextStatement(80)
-        s = Strategy(first, second)
+        s = Strategy.new(first, second)
         self.assertEqual(s.interpret({}), 60, 'interpret method of Strategy object should return 60')
 
         second = None
-        s = Strategy(first, second)
+        s = Strategy.new(first, second)
         self.assertEqual(s.interpret({}), 60, 'interpret method of Strategy object should return 60')
 
     def test_interpret_none_first_statement(self):
         first = FirstStatement(None)
         second = NextStatement(80)
-        s = Strategy(first, second)
+        s = Strategy.new(first, second)
         self.assertEqual(s.interpret({}), 80, 'interpret method of Strategy object should return 80')
 
     def test_interpret_both_none(self):
         first = FirstStatement(None)
         second = None
-        s = Strategy(first, second)
+        s = Strategy.new(first, second)
         self.assertEqual(s.interpret({}), None, 'interpret method of Strategy object should return None')
 
 
