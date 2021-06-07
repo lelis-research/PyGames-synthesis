9dc2e6b2be4e64029a8df3e9016c88ac2f628c53
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-07 13:44:25 -0600

    Add evaluate method to Evaluation class in evaluation.py
     - The part where the synthesized program plays the Catcher game can be
       removed from the is_correct and made into its own method called
       evaluate.
    
     - This is useful because algorithms like simulated annealing only needs
       the score of the synthesized program. Unlike for the BUS synthesizer,
       it does not need to know whether the program is 'correct' or not.

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -30,26 +30,40 @@ class Evaluation:
         env['actions'] = action_set
         return env
 
-    def is_correct(self, program):
+    def evaluate(self, program):
         """
-        The is_correct method evaluates a generated program passed in as parameter.
-        This method basically initializes a Catcher game and every time an action is
-        required by the game, the synthesized program's interpret method is called
-        and the returned action is passed on to the act method of the PLE object.
+        The evaluate method runs a Catcher game and uses the program parameter as
+        strategy to determine which actions to take at each game tick. It then
+        returns the score of the program when the game is over or when an exception
+        is raised due to an impossible action.
         """
         game = Catcher(width=500, height=500, init_lives=3)
         p = PLE(game, fps=30, display_screen=False)
 
-        while not p.game_over() and p.score() < self.score_threshold:
+        score = -100000
+        while not p.game_over():
             env = self.update_env(p.getGameState(), p.getActionSet())
             try:
                 action = program.interpret(env)
             except:
-                return False, 0
+                return 0
             p.act(action)
+            score = p.score()
+
+        return score
+
+    def is_correct(self, program):
+        """
+        This method calls the evaluate method which returns the score of the program
+        in a Catcher game. It then uses the score to determine if the program is
+        'correct', that is if the latter's score is greater than or equal to the
+        self.score_threshold attribute. If yes, it returns True and the score of the
+        program. Otherwise, it returns False and the score.
+        """
+        score = self.evaluate(program)
         
-        if p.score() < self.score_threshold:
-            return False, p.score()
+        if score < self.score_threshold:
+            return False, score
         else:
-            self.score_threshold = p.score()
-            return True, p.score()
+            self.score_threshold = score
+            return True, score
