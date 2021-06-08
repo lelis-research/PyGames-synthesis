95923a54bd9c62f8fbfb6e66d2dd196cb09dd96d
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-08 11:28:09 -0600

    Add start_search.py file to the SA synthesizer directory
     - Like for the BUS synthesizer directory, the driver code for the SA
       synthesizer can be made into a function which is called by the
       top-level driver code in the src directory.
    
     - The function in the start_search.py file can then set up the
       environment for the SA synthesizer specifically.

---
+++ src/SA/start_search.py
@@ -0,0 +1,40 @@
+"""
+start_search.py 
+
+Author: Olivier Vadiavaloo
+
+Description:
+This module implements the driver code of the Simulated Annealing synthesizer.
+
+It declares the operators, dsfs, constants and scalars to be used during
+the synthesis process, and calls the synthesizer with the desired arguments.
+
+"""
+from src.SA.sim_anneal import *
+from src.evaluation import *
+
+def start_sa(time_limit, log_file):
+    grammar = {}
+    grammar['operators'] = [Plus, 
+                            Minus,
+                            Times, 
+                            Divide, 
+                            LessThan, 
+                            GreaterThan, 
+                            EqualTo, 
+                            IT, 
+                            ITE, 
+                            Strategy, 
+                            ReturnAction
+                        ]
+
+    grammar['dsfs'] = [FallingFruitPosition, PlayerPosition]
+    grammar['constants'] = np.arange(0, 101, 0.01).tolist()
+    grammar['scalars'] = ['paddle_width']
+    grammar['arrays'] = ['actions']
+    grammar['array_indexes'] = [0, 1, 2]
+
+    sa = SimulatedAnnealing(time_limit, log_file)
+
+    eval_funct = Evaluation(0)
+    sa.synthesize(grammar, 2000, 1, eval_funct)
