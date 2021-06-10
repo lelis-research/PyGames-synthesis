615a8f6ccbe5527e5248ecee91b037a6ddba89f2
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-09 22:53:44 -0600

    Implement triage and non-triage optimization
     - By using the suggest, evaluate and register paradigm, the internals
       of the maximize method of the bayes optimizer can be reproduced.
    
     - This allows one to implement an optimize process that uses triage.
    
     - The non-triage version uses the maximize method.

--- src/Optimizer/optimizer.py
+++ src/Optimizer/optimizer.py
@@ -8,22 +8,21 @@ This module provides an Optimizer class that can be used by
 synthesizers such as Bottom-Up Search to find the optimal value
 of operands/parameters in operations defined in the DSL.
 """
-from numpy.lib.arraysetops import isin
 from src.dsl import *
 from src.evaluation import Evaluation
-from scipy import spatial
-from bayes_opt import BayesianOptimization
-from bayes_opt.logger import JSONLogger
-from bayes_opt.event import Events
-from bayes_opt.util import load_logs
+from bayes_opt import BayesianOptimization, UtilityFunction
 import numpy as np
 import os
 
 class Optimizer:
 
-    def __init__(self, eval_funct, is_triage):
+    def __init__(self, eval_funct, is_triage, iterations):
         self.eval_funct = eval_funct
         self.is_triage = is_triage
+        self.iterations = iterations
+        if is_triage:
+            self.iter_breakdown = self.break_down(iterations)
+
         self.log_file = './logs.json'
 
     def get_const_range(self):
@@ -79,42 +78,79 @@ class Optimizer:
         score = self.eval_funct.evaluate(self.ast)
         return score
 
+    def break_down(self, iterations):
+        iter_breakdown = []
+        for i in [1, 5, 15, 30, 49]:
+            iter_breakdown.append(int((i / 100) * iterations))
+
+        return iter_breakdown.copy()
+
+    def triage_optimize(self):
+        bayesOpt = BayesianOptimization(
+            f=None,
+            pbounds=self.const_range_list,
+            verbose=0
+        )
+
+        utility = UtilityFunction(kind='ucb', kappa=2.5, xi=0.0)
+
+        current_max_score = self.initial_score
+        current_argmax_params = self.original_values
+        is_optimized = True
+        for i in self.iter_breakdown:
+            # Run Bayesian Optimization
+            for _ in range(i):
+                next_point = bayesOpt.suggest(utility)
+                target = self.evaluation_fun(next_point)
+                bayesOpt.register(params=next_point, target=target)
+
+            # Compare results with previous runs of the optimizer
+            target, params = bayesOpt.max['target'], bayesOpt.max['params']
+            if target < current_max_score:
+                is_optimized = False
+                break
+
+            current_max_score = target
+            current_argmax_params = params
+
+        self.set_const_value(current_argmax_params)
+        return current_argmax_params, current_max_score, is_optimized
+
+    def non_triage_optimize(self):
+        bayesOpt = BayesianOptimization(
+            f=self.evaluation_fun,
+            pbounds=self.const_range_list,
+            verbose=0
+        )
+
+        bayesOpt.maximize(init_points=20, n_iter=self.iterations)
+        target, params = bayesOpt.max['target'], bayesOpt.max['params']
+        if target < self.initial_score:
+            is_optimized = False
+            params = self.original_values
+            target = self.initial_score
+
+        self.set_const_value(params)
+        return bayesOpt.max['params'], bayesOpt.max['target'], is_optimized
+
     def optimize(self, ast, initial_ast_score):
         self.ast = ast
+        self.initial_score = initial_ast_score
 
-        const_range_list, original_values = self.get_const_range()
-        if len(original_values) == 0:
-            return original_values, 0, False
+        self.const_range_list, self.original_values = self.get_const_range()
+        if len(self.original_values) == 0:
+            return self.original_values, 0, False
 
         if os.path.exists(self.log_file):
             os.remove(self.log_file)
         else:
             print(f'{self.log_file} does not exist')
 
-        bayesOpt = BayesianOptimization(
-            self.evaluation_fun,
-            pbounds=const_range_list,
-            verbose=0
-        )
-
-        logger = JSONLogger(path=self.log_file)
-        bayesOpt.subscribe(Events.OPTIMIZATION_STEP, logger)
-
         try:
-            # Perform triage-based optimization if is_triage set to True
-            current_score = initial_ast_score
             if self.is_triage:
-                for n in [5, 10, 25, 60, 100]:
-                    bayesOpt.maximize(init_points=20, n_iter=n)
-                    assert bayesOpt.max['target'] < current_score
-                    current_score = bayesOpt.max['target']
-                    load_logs(bayesOpt, logs=[self.log_file])
+                return self.triage_optimize()
             else:
-                bayesOpt.maximize(init_points=20, n_iter=200)
-                current_score = bayesOpt.max['target']
-            
-            self.set_const_value(bayesOpt.max['params'])
-            return bayesOpt.max['params'], current_score, True
+                return self.non_triage_optimize()
         except:
-            self.set_const_value(original_values)
-            return original_values, initial_ast_score, False
+            self.set_const_value(self.original_values)
+            return self.original_values, self.initial_score, False
