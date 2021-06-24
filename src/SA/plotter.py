b63566507a35a56c6028211a2df76bc7f70721e6
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-24 16:38:21 -0600

    Implement save_data and plot_from_file methods
     - With save_data and plot_from_file methods, the plotter object can
       retrieve data from previous runs and plot them on a graph using the
       matplotlib module.
    
     - Since the data is in 'raw' format, it is very versatile and can
       easily used with other types of graph plotters.

--- src/SA/plotter.py
+++ src/SA/plotter.py
@@ -9,6 +9,8 @@ This module implements
 import src.Utils.plotter as base_plt
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from os.path import join
 
 class Plotter(base_plt.Plotter):
 
@@ -21,6 +23,21 @@ class Plotter(base_plt.Plotter):
             for epoch in score_dict:
                 y.append(score_dict[epoch])
 
-        x = np.linspace(0, len(y), len(y), endpoint=True)
+        x = list(range(0, len(y)))
 
         return x, y
+
+    def save_data(self, *data, names=[]):
+        DATA_DIR = 'data/'
+        count = 0
+        for data_dict in data:
+            if not os.path.exists(DATA_DIR):
+                os.makedirs(DATA_DIR)
+
+            with open(join(DATA_DIR + names[count]), 'w') as data_file:
+                data_file.write(f'# {names[count]}')
+                x, y = self.parse_data(data_dict)
+                for i in range(len(y)):
+                    data_file.write(f'{x[i]} {y[i]}\n')
+
+            count += 1
