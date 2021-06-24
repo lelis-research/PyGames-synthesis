c9abd0a082e07947b36cb4df05d1317eb156230c
olivier-vadiaval <vadiaval@ualberta.ca>
2021-06-24 16:42:53 -0600

    Add plot_from_file method

--- src/Utils/plotter.py
+++ src/Utils/plotter.py
@@ -10,21 +10,23 @@ of scores over time.
 """
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 class Plotter:
 
     def parse_data(self):
         raise Exception('parse_data method not implemented')
 
-    def save_raw(self):
+    def save_data(self):
         raise Exception('save_raw method not implemented')
 
     def plot(self, x, y, title='', xlabel='x', ylabel='y'):
         plt.plot(x, y)
         plt.title(title)
         plt.xlabel(xlabel)
+        plt.xticks(list(range(x[0], x[-1] + 1, int(0.1 * len(x)))))
         plt.ylabel(ylabel)
-        plt.savefig(title.replace(' ', '_') + '.jpg')
+        plt.savefig(title.replace(' ', '_') + '.png')
 
     def plot_from_data(self, *data, names={}):
         for data_dict in data:
@@ -35,8 +37,27 @@ class Plotter:
             
             plt.plot(x, y)
 
+        x_len = len(x)
         plt.xlabel(names['x'])
         plt.ylabel(names['y'])
         plt.legend(names['legend'], loc='lower right')
         plt.title(names['title'])
-        plt.savefig(names['title'].replace(' ', '_') + '.pdf', dpi=300, bbox_inches='tight')
+        plt.savefig(names['title'].replace(' ', '_') + '.png')
+
+    def plot_from_file(self, path, names={}):
+        if not os.path.exists(path):
+            raise Exception(f'Path to {path} does not exist')
+
+        with open(path, 'r') as data_file:
+            x = []
+            y = []
+            lines = data_file.readlines()
+            for line in lines:
+                if line[0] == '#':
+                    continue
+                
+                line_split = line.split()
+                x.append(int(line_split[0]))
+                y.append(round(float(line_split[1]), 2))
+
+            self.plot(x, y, title=names['title'], xlabel=names['x'], ylabel=names['y'])
