c0b93d24a45e9da86cd0884d2cfd95cefadea2fa
olivier-vadiaval <vadiaval@ualberta.ca>
2021-07-02 16:47:50 -0600

    Create DslConfig class to parse JSON config file
     - The valid dsfs across different domains/games can vary and so, the
       proper configuration of the dsl can be stored in a JSON and parsed
       bwfore starting the synthesizer.
    
     - The DslConfig class parses the JSON config file and constructs the
       grammar object.

---
+++ src/Utils/dsl_config.py
@@ -0,0 +1,42 @@
+"""
+dsl_config.py
+
+Author: Olivier Vadiaval
+
+Description:
+This module implements the class that will load a game's dsl
+from a json config file.
+"""
+import json
+import os
+from src.dsl import *
+
+class DslConfig:
+
+    def __init__(self, config_filepath):
+        if not os.path.exists(config_filepath):
+            raise Exception('Path to config file is invalid')
+        
+        self.config_filepath =  config_filepath
+
+    def get_grammar(self, game):
+        with open(self.config_filepath) as config_f:
+            dsl_dict = json.load(config_f)
+
+            grammar = {}
+            grammar['dsfs'] = []
+            for dsf in dsl_dict[game]['dsfs']:
+                grammar['dsfs'].append(Node.get_class(dsf))
+
+            print('grammar[\'dsfs\']', grammar['dsfs'])
+
+            grammar['operators'] = []
+            for op in dsl_dict['operators']:
+                grammar['operators'].append(Node.get_class(op))
+
+            grammar['scalars'] = dsl_dict['scalars']
+            grammar['arrays'] = dsl_dict['arrays']
+            grammar['array_indexes'] = dsl_dict[game]['array_indexes']
+            grammar['constants'] = np.arange(0, 101, 0.01).tolist()
+
+            return grammar
