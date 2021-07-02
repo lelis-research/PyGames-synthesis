281b599b0110e9eea8e9cd2e4c05f632a16fb42a
olivier-vadiaval <vadiaval@ualberta.ca>
2021-07-02 11:41:10 -0600

    Create factory for Evaluation object
     - The evaluation objects can be different depending on the games being
       used for the evaluation.
    
     - Hence, it makes sense to create a factory object that returns the
       proper Evaluation object depending on the game being used.
    
     - To make the code more flexible, also use inheritance by creating
       different Evaluation sub-classes for each game.

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -10,90 +10,97 @@ Catcher game implemented in
 https://pygame-learning-environment.readthedocs.io/en/latest/user/games/catcher.html
 
 """
+import copy as cp
 import pygame, time
+from pygame.locals import *
+from pygame.version import ver
 
 ### import game from Pygame-Learning-Environment ###
 from pygame_games.ple.games.catcher import Catcher
-from pygame_games.ple.games.pong import Pong
+# from pygame_games.ple.games.pong import Pong
 from pygame_games.ple.ple import PLE
+from pong.pong3 import Pong
+
+# keys: game
+# values: number of players
+available_games = {'Catcher': 1, 'Pong': 2}
+
+class EvaluationFactory:
+
+    def __init__(self, threshold):
+        self.set_games_dict(threshold)
+
+    def set_games_dict(self, threshold):
+        self.games_dict = {
+            'Catcher': EvaluationCatcher(threshold), 
+            'Pong': EvaluationPong(threshold)
+        }
+
+    def get_eval_fun(self, eval_str):
+        return self.games_dict[eval_str]
 
 class Evaluation:
 
-    games_dict = {
-        'Catcher': Catcher(width=500, height=500, init_lives=3), 
-        'Pong': Pong(width=600, height=550, MAX_SCORE=5)
-    }
-    available_games = list(games_dict.keys())
+    def __init__(self, score_threshold):
+        self.score_threshold = score_threshold
 
-    def __init__(self, threshold, game):
-        self.score_threshold = threshold
-        self.gamename = game
-        self.game = Evaluation.games_dict[game]
+    def get_score(self):
+        raise Exception('Must implement get_score method')
 
-    def update_env(self, game_state, action_set):
-        """
-        This method updates the env variable based on the game_state and
-        the action set values.
-        """
-        env = {}
-        env['state'] = {}
-        if self.gamename == 'Catcher':
-            env['state']['non_player_position'] = game_state.get('fruit_x')
-            env['state']['non_player_approaching'] = True
-        else:
-            env['state']['non_player_position'] = game_state.get('ball_y')
-            env['state']['non_player_approaching'] = game_state.get('ball_velocity_x') < 0
-        
-        env['state']['player_position'] = game_state.get('player_x')
-        env['paddle_width'] = game_state.get('paddle_width')
-        env['actions'] = action_set
-        return env
+    def game_over(self):
+        raise Exception('Must implement game_over method')
+
+    def play(self):
+        raise Exception('Must implement play method')
+
+    def update_env(self):
+        raise Exception('Must implement update_env method')
+    
+    def reset_game(self):
+        raise Exception('Must implement reset_game method')
+
+    def set_best(self, best, best_eval):
+        pass
 
     def evaluate(self, program, optimizing=False, verbose=False):
         """
-        The evaluate method runs a Catcher game and uses the program parameter as
+        The evaluate method runs a game and uses the program parameter as
         strategy to determine which actions to take at each game tick. It then
         returns the score of the program when the game is over or when an exception
         is raised due to an impossible action.
         """
-        p = PLE(self.game, fps=30, display_screen=True, rng=int(time.time()))
-        scores = []
         if optimizing:
             total_games_played = 5
         else:
-            total_games_played = 35
-
-        for i in range(total_games_played):
-            score = -100000
-            while not p.game_over():
-                env = self.update_env(p.getGameState(), p.getActionSet())
+            total_games_played = 1
+        
+        scores = []
+        score = -1_000_000
+        for _ in range(total_games_played):
+            self.reset_game()
+            while not self.game_over():
                 try:
-                    action = program.interpret(env)
+                    score = self.play(program)
                 except:
                     if verbose:
                         return tuple([]), -1_000_000
                     else:
                         return -1_000_000
-            
-                p.act(action)
-                score = p.score()
-
-            p.reset_game()
+                        
             scores.append(score)
-
-        score_avg = round(sum(scores) / total_games_played, 2)
+        
+        score_avg = round(sum(scores) // total_games_played, 2)
         if verbose:
-            return (tuple(scores), score_avg)
+            return tuple(scores), score_avg
         else:
             return score_avg
 
     def is_correct(self, program):
         """
-        This method calls the evaluate method which returns the score of the program
-        in a Catcher game. It then uses the score to determine if the program is
-        'correct', that is if the latter's score is greater than or equal to the
-        self.score_threshold attribute. If yes, it returns True and the score of the
-        program. Otherwise, it returns False and the score.
+        The evaluate method runs a game and uses the program parameter as
+        strategy to determine which actions to take at each game tick. It then
+        returns the score of the program when the game is over or when an exception
+        is raised due to an impossible action.
         """
         score = self.evaluate(program)
         
@@ -102,3 +109,96 @@ class Evaluation:
         else:
             self.score_threshold = score
             return True, score
+
+
+class EvaluationPong(Evaluation):
+
+    def __init__(self, reward_threshold):
+        self.best = None
+        self.best_eval = None
+        super().__init__(reward_threshold)
+
+    def set_best(self, best, best_eval):
+        self.best = best
+        self.best_eval = best_eval
+    
+    def get_best(self):
+        return self.best, self.best_eval
+
+    def update_env(self, player, game_state, action_set):
+        env = {}
+        env['state'] = {}
+        env['state']['non_player_position'] = game_state['ball_y']
+
+        if player == 'p1':
+            env['state']['non_player_approaching'] = game_state['ball_vel'] < 0
+        else:
+            env['state']['non_player_approaching'] = game_state['ball_vel'] > 0
+
+        env['state']['player_position'] = game_state[player + '_pos']
+        env['paddle_width'] = game_state['paddle_width']
+        env['actions'] = action_set
+        return env
+
+    def get_score(self):
+        return self.game.get_rewards()[0]   # return rewards of p1
+    
+    def reset_game(self):
+        self.game = Pong()
+
+    def game_over(self):
+        return (not self.game.continue_game or self.game.close_clicked)
+
+    def play(self, program):
+        actions = []
+        p1 = program
+
+        if self.best is not None:
+            p2 = self.best
+        else:
+            p2 = cp.deepcopy(p1)
+
+        env = self.update_env('p1', self.game.game_state(), [K_q, K_a, None])
+        actions.append(p1.interpret(env))
+        env = self.update_env('p2', self.game.game_state(), [K_p, K_l, None])
+        actions.append(p2.interpret(env))
+
+        self.game.step(actions)
+        return self.get_score()
+
+
+class EvaluationCatcher(Evaluation):
+
+    def __init__(self, score_threshold):
+        super(EvaluationCatcher, self).__init__(score_threshold)
+
+    def update_env(self, game_state, action_set):
+        """
+        This method updates the env variable based on the game_state and
+        the action set values.
+        """
+        env = {}
+        env['state'] = {}
+        env['state']['non_player_position'] = game_state.get('fruit_x')
+        env['state']['non_player_approaching'] = True
+        
+        env['state']['player_position'] = game_state.get('player_x')
+        env['paddle_width'] = game_state.get('paddle_width')
+        env['actions'] = action_set
+        return env
+
+    def get_score(self):
+        return self.game.getScore()
+
+    def game_over(self):
+        return self.game.game_over()
+
+    def reset_game(self):
+        self.game = Catcher(width=500, height=500, init_lives=3)
+        self.p = PLE(self.game, fps=30, display_screen=True, rng=int(time.time()))
+
+    def play(self, program):
+        env = self.update_env(self.p.getGameState(), self.p.getActionSet())
+        action = program.interpret(env)
+        self.p.act(action)
+        return self.p.score()
