d893c758b52bb5f0a1f76f5336cb3ea331dbb7ee
olivier-vadiaval <vadiaval@ualberta.ca>
2021-07-12 11:15:52 -0600

    Save optimized scores only when optimizer was used
     - In previous versions, the save() would try to save the optimized
       scores even though the optimizer wasn't used during the search.
    
     - This raises an error on saving because the dictionary that normally
       stores optimized scores is empty.

--- src/SA/sim_anneal.py
+++ src/SA/sim_anneal.py
@@ -375,6 +375,7 @@ class SimulatedAnnealing:
 
     def save(self, plot_filename):
         plotter = Plotter()
+
         # Save data to files
         data_filenames = [
             'score_variances_' + plot_filename.replace('graph', 'data') + '.dat',
@@ -384,20 +385,24 @@ class SimulatedAnnealing:
             'optimized_scores_' + plot_filename.replace('graph', 'data') + '.dat'
         ]
 
-        # plotter.save_data(
-        #     self.scores_variance_dict,
-        #     self.scores_dict, 
-        #     self.best_pscore_dict, 
-        #     self.unoptimized_pscore_dict,
-        #     self.optimized_pscore_dict,
-        #     names=data_filenames
-        # )
+        if self.run_optimizer:
+            plotter.save_data(
+                self.scores_variance_dict,
+                self.scores_dict, 
+                self.best_pscore_dict, 
+                self.unoptimized_pscore_dict,
+                self.optimized_pscore_dict,
+                names=data_filenames
+            )
 
-        plotter.save_data(
-            self.scores_variance_dict,
-            names=data_filenames
-        )
-
+        else:
+           plotter.save_data(
+                self.scores_variance_dict,
+                self.scores_dict, 
+                self.best_pscore_dict,
+                names=data_filenames
+            )
+ 
 
     def plot(self, plot_filename):
         plotter = Plotter()     # Plotter object
@@ -411,24 +416,6 @@ class SimulatedAnnealing:
         }
 
         plotter.plot_from_data(self.scores_dict, self.best_pscore_dict, names=plot_names)     # plot all scores
-
-        # Plot scores of solutions generated during first run of SA
-        # for i in range(len(self.scores_dict.keys())):
-        #     x = list(self.scores_dict[i].keys())
-        #     y = list(self.scores_dict[i].values())
-
-              # Match i to its corresponding string description
-              # That is, if i = 0 and this means that the scores
-              # for the first iteration will be plotted and so '1st'
-              # is i's string description and is used in the title
-        #     i_strings = ['st', 'nd', 'rd', 'th']
-        #     i_str = str(i+1) + i_strings[min(i, 3)]
-
-        #     plot_names['title'] = f'{i_str} SA Run - Program Scores vs Iterations'
-        #     plot_names['filename'] = plot_filename + f'_{i_str}_SA_Run'
-        #     plot_names['x'] = 'iterations'
-
-        #     plotter.plot(x, y, plot_names)
 
     def simulated_annealing(
             self,
