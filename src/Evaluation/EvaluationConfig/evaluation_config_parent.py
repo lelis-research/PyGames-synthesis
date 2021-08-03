6d33a659f6fcb5a29656f8420ad02271bc314c3c
olivier-vadiaval <vadiaval@ualberta.ca>
2021-08-03 12:33:09 -0600

    Remove slack method and modify check_triage_stop
     - Remove the slack method that was used in the check_triage_stop.
    
     - Also, modify the check_triage_stop method by using Hoeffding's
       inequality to create a stopping condition for the evaluaiton.
    
     - If we're confident enough that the current program is weaker than the
       current best, then the evaluation is stopped.

--- src/Evaluation/EvaluationConfig/evaluation_config_parent.py
+++ src/Evaluation/EvaluationConfig/evaluation_config_parent.py
@@ -15,6 +15,7 @@ The EvaluationConfig class is used by the Evaluation class which implements the 
 logic at a higher level.
 """
 from statistics import *
+import math
 
 class EvaluationConfig:
 
@@ -23,6 +24,8 @@ class EvaluationConfig:
     min_score_name = 'MIN_SCORE'
     triage_name = 'triage'
     batch_size_name = 'batch_size'
+    confidence_value_name = 'confidence_value'
+    triage_var_bound_name = 'triage_var_bound'
 
     def __init__(self, attributes):
         self.config_attributes_set = False
@@ -30,6 +33,8 @@ class EvaluationConfig:
 
     def set_config_attributes(self, attributes):
         self.triage = attributes[self.triage_name]
+        self.triage_confidence_value = attributes[self.confidence_value_name]
+        self.triage_random_var_bound = attributes[self.triage_var_bound_name]
         self.total_games = attributes[self.total_games_name]
         self.best_eval = attributes[self.best_eval_name]
         self.MIN_SCORE = attributes[self.min_score_name]
@@ -58,12 +63,11 @@ class EvaluationConfig:
     def get_best_eval(self):
         return self.best_eval
 
-    def slack(self, games_played):
-        slack_value = self.best_eval * (((self.total_games - games_played) * 1.75) / self.total_games)
-        if slack_value < 0:
-            slack_value *= -1
+    def get_random_var_bound(self):
+        return self.triage_random_var_bound
 
-        return slack_value
+    def get_confidence_value(self):
+        return self.triage_confidence_value
 
     def clean_up(self):
         pass
@@ -71,8 +75,40 @@ class EvaluationConfig:
     def compute_results(self, scores, games_played):
         raise Exception('Must implement compute_results method')
 
-    def check_continue(self, games_played):
+    def check_continue(self, program_current_score, games_played):
         raise Exception('Must implement check_continue method')
 
-    def check_triage_stop(self, games_played):
-        raise Exception('Must implement check_triage_stop')
+    def check_triage_stop(self, program_current_score, games_played):
+
+        def compute_epsilon(number_evals):
+            return self.triage_random_var_bound * \
+                math.sqrt(math.log(2 / (1 - math.sqrt(self.triage_confidence_value))) / (2 * number_evals))
+
+        epsilon_current_best = compute_epsilon(self.total_games)
+        epsilon_program = compute_epsilon(games_played)
+
+        # print('CI', self.triage_confidence_value)
+        # print('random var bound', self.triage_random_var_bound)
+
+        # print('games_played', games_played)
+        # print('current best score', self.best_eval)
+        # print('transform current best score', transform_current_best)
+        # print('epsilon current best score', epsilon_current_best)
+        # print('current best lower bound', self.best_eval - epsilon_current_best)
+        # print('current best lower bound (x500)', self.best_eval - (epsilon_current_best * 500))
+        # print('transform current best lb (100)', transform_current_best - (epsilon_current_best * 100))
+        # print()
+
+        # print('program score', program_current_score)
+        # print('transform program score', transform_program_curr_score)
+        # print('epsilon program score', epsilon_program)
+        # print('program score upper bound', program_current_score + epsilon_program)
+        # print('program score upper bound (x500)', program_current_score + (epsilon_program * 500))
+        # print('transform program score ub (100)', transform_program_curr_score + (epsilon_program * 100))
+        # print('=' * 20)
+        # print()
+
+        if (program_current_score + epsilon_program) <= (self.best_eval - epsilon_current_best):
+            return True
+
+        return False
