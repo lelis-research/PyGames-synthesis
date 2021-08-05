f398e8ab673a7f19e67c46d5157a702c51ba8915
Olivier <vadiaval@ualberta.ca>
2021-08-04 22:27:26 -0400

    Allow synthesizer to run in the background

--- bin/sa_no_opt_triage_eval.sh
+++ bin/sa_no_opt_triage_eval.sh
@@ -58,4 +58,4 @@ python -u -m src.main -t ${time} -l ${log_name} \
     -g ${game} -s SimulatedAnnealing --tg ${total_games} --te ${var_bound} ${confidence} \
     --plot --plot-name ${plot_name} --save --config ${config} \
     ${mr} \
-    --no-warn
+    --no-warn >> /dev/null 2>&1 &
